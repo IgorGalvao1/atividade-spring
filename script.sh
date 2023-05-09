73aa3cf92ac0a1a84bafdf927da0d3e6889e870c
Igor Galvão <102822602+IgorGalvao1@users.noreply.github.com>
2023-05-09 13:57:13 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-    if ! mysql -u root -e "CREATE DATABASE apllicationdb" &> /dev/null
+    if ! mysql -u root -e "CREATE DATABASE applicationdb" &> /dev/null
     then
         sleep 1
     else
